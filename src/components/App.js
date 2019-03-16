3b7121396b4e704feeeb572a357fb6202b9ac5bb
michaeldai1006 <dtc74110@gmail.com>
2019-03-16 11:39:55 -0700

    Creating Custom Clients

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+import upsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
     state = { images: [] };
 
     onSearchSubmit = async term => {
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID ********'
-            }
+        const response = await upsplash.get('/search/photos', {
+            params: { query: term }
         });
 
         this.setState({ images: response.data.results });
