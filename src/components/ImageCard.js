2b0d8653de5cb3275253c9cd4fd364d71771af19
michaeldai1006 <dtc74110@gmail.com>
2019-03-17 16:43:14 -0700

    Accessing the DOM with Refs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class ImageCard extends React.Component {
     render() {
-        const { description, urls } = this.props.iamge;
+        const { description, urls } = this.props.image;
 
         return (
             <div>
